391efe041fe1499cbe03194cd836db5830130b2a
Nmcleon <nmcleon10@gmail.com>
2024-04-28 16:06:27 +0200

    add Js

--- Navigation_animation/script.js
+++ Navigation_animation/script.js
@@ -1,11 +1,3 @@
-const labels = document.querySelectorAll('.form-conrol label');
-
-labels.forEach(label => {
-  label.innerHTML = label.innerText
-    .split('')
-    .map(
-      (letter, idx) =>
-        `<span style="transition-delay:${idx * 50}ms">${letter}</span>`
-    )
-    .join('');
+document.getElementById('toggle').addEventListener('click', function () {
+  document.getElementById('nav').classList.toggle('active');
 });
